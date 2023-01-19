29ba4bbe863ab7eac9ffcb2dcdf6c3cad16249fe
Idan Refua <idan6208@gmail.com>
2023-01-19 18:51:24 +0200

    add likes and delete route

--- Server/routes/productsRoutes.js
+++ Server/routes/productsRoutes.js
@@ -42,10 +42,69 @@ router.get("/myproducts", authMiddleWare, async (req, res) => {
     const user = req.userData;
     if (user.admin) {
       const userProducts = await productsModel.userProducts(user._id);
-      console.log(req.userData._id);
       res.status(200).json(userProducts);
     } else {
       throw "You are not admin user";
+    }
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
+
+router.delete("/deleteproduct/:productid", authMiddleWare, async (req, res) => {
+  try {
+    const productId = req.params.productid;
+    const user = req.userData;
+    if (user.admin) {
+      const product = await productsModel.productById(productId);
+      if (product.createdBy === user._id) {
+        const deleteProduct = await productsModel.deleteProduct(productId);
+        res.status(200).json({ message: "The product delete succssfuly" });
+      } else {
+        throw "This is not your product";
+      }
+    } else {
+      throw "You are not admin user";
+    }
+  } catch (error) {
+    res.status(401).json({ error });
+  }
+});
+
+////
+/////
+//////
+//////
+
+router.patch("/updateproduct/:id", authMiddleWare, async (req, res) => {
+  try {
+    const user = req.userData;
+    const productId = req.params.id;
+    const body = req.body;
+    if (user.admin) {
+      const product = await productsModel.productById(productId);
+      if (product.createdBy === user._id) {
+        const updateProduct = await productsModel.updateProduct(productId);
+        res.status(200).json(updateProduct);
+      }
+    } else {
+      throw "You are not admin user";
+    }
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
+
+router.post("/addtofavourites/:id", authMiddleWare, async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const user = req.userData;
+    const product = await productsModel.productById(productId);
+    if (!product.likes.includes(user._id)) {
+      const addLike = await productsModel.addToFavourites(productId, user._id);
+      res.status(200).json(addLike);
+    } else {
+      throw "You already add this product to yoru favourites list";
     }
   } catch (error) {
     res.status(400).json({ error });
