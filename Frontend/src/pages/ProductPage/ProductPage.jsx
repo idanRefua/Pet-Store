37b32b7678b43b2e9fd9d05c52f6552b341013ec
Idan Refua <idan6208@gmail.com>
2023-01-22 17:08:55 +0200

    add product page and admin routes guard

--- Frontend/src/pages/ProductPage/ProductPage.jsx
+++ Frontend/src/pages/ProductPage/ProductPage.jsx
@@ -1,3 +1,24 @@
+import { useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
 export default function ProductPage() {
+  const history = useHistory();
+  const [product, setProduct] = useState();
+  const id = useParams();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const product = await axios.get(`/products/moreinfo/${id}
+        `);
+        const productData = await product.data();
+        setProduct(productData);
+      } catch (error) {
+        history.push("*");
+      }
+    };
+    fetchData();
+  }, []);
+
   return <div></div>;
 }
