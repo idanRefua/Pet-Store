73a10d25d5eadf35f685a7dfbcd83ae7ffbbb1ea
Idan Refua <idan6208@gmail.com>
2023-01-25 19:46:24 +0200

    fix reviews on product page

--- Frontend/src/pages/ProductPage/ProductPage.jsx
+++ Frontend/src/pages/ProductPage/ProductPage.jsx
@@ -3,11 +3,15 @@ import { Fragment, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
 import { useState } from "react";
-
+import { useSelector } from "react-redux";
 export default function ProductPage() {
   const history = useHistory();
-  const [product, setProduct] = useState(null);
   const { id } = useParams();
+  const userInfo = useSelector((state) => state.auth.userInfo);
+  const [review, setReview] = useState();
+  const [product, setProduct] = useState(null);
+  const [productsReviews, setProductsReviews] = useState([]);
+  const [productLikes, setProductLikes] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -15,13 +19,30 @@ export default function ProductPage() {
         `);
         const productData = await product.data;
         setProduct(productData);
-        console.log(productData);
+        setProductsReviews(productData.reviews);
+        setProductLikes(productData.likes);
       } catch (error) {
         history.push("/notfoundpage");
       }
     };
     fetchData();
-  }, [id]);
+  }, [id, productsReviews]);
+
+  const handleAddReview = async () => {
+    try {
+      const reviewByUser = await axios.post(`/products/addreview/${id}`, {
+        review,
+      });
+      const dataReview = await reviewByUser.data;
+      console.log(dataReview);
+    } catch (error) {
+      alert("can't add review right not please try again later!");
+    }
+  };
+
+  const hadnleReviewText = (e) => {
+    setReview(e.target.value);
+  };
 
   return (
     <div>
@@ -38,9 +59,40 @@ export default function ProductPage() {
             <div className="col-sm-6 ">
               <h3 className="product-page-title">{product.title}</h3>
               <p className="product-page-p">{product.description}</p>
+              <br />
+              <br />
+              <br />
+              <br />
+              <p>
+                {!productLikes.includes(userInfo._id) && (
+                  <button>Add To Favourites</button>
+                )}
+                {productLikes.includes(userInfo._id) && (
+                  <button>Remove From Favourites</button>
+                )}
+              </p>
             </div>
           </div>
           <div className="container reviews-table">
+            {!productsReviews.some(
+              (review) => review.byUser === userInfo._id
+            ) && (
+              <div className="review-input">
+                <textarea
+                  type="text"
+                  value={review}
+                  name=""
+                  id="product-review"
+                  rows={3}
+                  cols={120}
+                  onChange={hadnleReviewText}
+                />
+                <br />
+                <button onClick={handleAddReview}>Add Review</button>
+              </div>
+            )}
+
+            <br />
             <table className="table">
               <thead>
                 <tr>
@@ -53,10 +105,18 @@ export default function ProductPage() {
                 </tr>
               </thead>
               <tbody>
-                <tr>
+                {productsReviews.map((review) => {
+                  return (
+                    <tr>
+                      <td>{review.userName}</td>
+                      <td>{review.review}</td>
+                    </tr>
+                  );
+                })}
+                {/*  <tr>
                   <td>Otto</td>
                   <td>@mdo</td>
-                </tr>
+                </tr> */}
               </tbody>
             </table>
           </div>
