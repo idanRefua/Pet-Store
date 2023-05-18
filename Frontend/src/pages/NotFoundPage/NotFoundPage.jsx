3bf60c48fe071676dd90b2f40e4c0114b745b9dd
Idan Refua <idan6208@gmail.com>
2023-05-18 18:06:48 +0300

    add not found page

--- Frontend/src/pages/NotFoundPage/NotFoundPage.jsx
+++ Frontend/src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,7 +1,21 @@
+import notFoundPageImage from "../../imgs/not-found-404.jpg";
+import "./not-found-page-style.css";
 export default function NotFoundPage() {
   return (
-    <div>
-      <h3>Welcome to not found page</h3>
+    <div className="container">
+      <br />
+      <br />
+      <div className="d-flex justify-content-center">
+        <img
+          className=" img-fluid not-found-image "
+          src={notFoundPageImage}
+          alt="not found image - 404"
+        />
+      </div>
+      <h1 className="d-flex justify-content-center not-found-title">404</h1>
+      <h1 className="d-flex justify-content-center not-found-title">
+        Oops something go wrong...
+      </h1>
     </div>
   );
 }
