555016557a1ae339eda279f9b126169f42d8d795
Idan Refua <idan6208@gmail.com>
2023-10-25 16:48:12 +0300

    my favourites products array fix

--- Frontend/src/pages/MyFavouritesPage/MyFavouritesPage.jsx
+++ Frontend/src/pages/MyFavouritesPage/MyFavouritesPage.jsx
@@ -1,20 +1,35 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./my-favourites-page-style.css";
 import axios from "axios";
+import ProductComponent from "../../components/ProductComponent/ProductComponent";
 const MyFavouritesPage = () => {
+  const [products, setProducts] = useState([]);
   useEffect(() => {
     axios
       .get("/products/myfavourites")
-      .then((res) => console.log(res.data))
+      .then((res) => setProducts(res.data))
       .catch((err) => console.log(err));
   }, []);
 
   return (
     <div className="container">
-      <h1 className="d-flex justify-content-center favourites-title">
+      <h1 className="d-flex justify-content-center favourites-title mt-3">
         This is Your Favourites Products
       </h1>
-      MyFavouritesPage
+
+      <div className="row mt-5">
+        {products.map((product) => {
+          return (
+            <ProductComponent
+              key={product._id}
+              id={product._id}
+              title={product.title}
+              price={product.price}
+              image={product.image}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
